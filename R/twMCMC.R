6ae19785c0cdfa4ae02994a0b6b6d5f368f75225
Alexander W Blocker <ablocker@gmail.com>
2011-12-26 22:12:59 -0500

    Roxygen2 run

--- R/twMCMC.R
+++ R/twMCMC.R
@@ -20,7 +20,7 @@
 #' @keywords models multivariate
 #' @export
 #' @examples
-#' cat("Example to come -- this is meant to annoy you into writing it",
+#' cat("Example to come -- this is meant to annoy you into writing it\n",
 #'     file=stderr())
 twMCMC <- function(Y, A, prior, ndraws=1.2e5, burnin=2e4, verbose=0) {
     # Format verification
