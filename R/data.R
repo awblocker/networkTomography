9e62dbf9b1aab5ddf0ae53cac328e6da2e2d3a91
Alexander W Blocker <ablocker@gmail.com>
2011-12-28 01:56:00 -0500

    Create formatted, organized 1router data archive; further documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+# Data documentation for Roxygen
+
+#' 1router data from Cao et al. (2000)
+#'
+#' Data from 4-node network with star topology collected from Bell Labs. Used in
+#' Cao et al. (2000).
+#'
+#' @docType data
+#' @name 1router
+#' @usage 1router
+#' @keywords datasets
+NULL
