cf3ac93c652883ddbcc3b89c7b9f287b45c41754
Alexander W Blocker <ablocker@fas.harvard.edu>
2012-11-04 22:41:24 -0500

    Add Abilene and CMU datasets. Small corrections to bell.labs documentation.

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' Cao et al. (2000).
 #'
 #' @section Objects:
-#' This the list bell.labs, which contains several objects:
+#' The list bell.labs, which contains several objects:
 #' \itemize{
 #'  \item \code{A}, the routing matrix for this network (truncated for full row
 #'      rank)
@@ -53,3 +53,93 @@
 #' @family bell.labs
 NULL
 
+#' CMU data from Blocker & Airoldi (2011)
+#'
+#' Data from the 12 node CMU network used in Blocker & Airoldi (2011). The OD
+#' flows are actual, observed traffic from a CMU network. The topology does not,
+#' however, correspond to the original network due to security considerations.
+#'
+#' @section Objects:
+#' The list cmu, which contains several objects:
+#' \itemize{
+#'  \item \code{A}, the routing matrix for this network (truncated for full row
+#'      rank)
+#'  \item \code{X}, a matrix of origin-destination flows formatted for analysis
+#'  \item \code{Y}, a matrix of link loads formatted for analysis
+#'  \item \code{A.full}, the routing matrix for this network without
+#'  truncatation for full row rank)
+#'  \item \code{Y.full}, a matrix of link loads corresponding to code{A.full}
+#' }
+#' In this data, we have \code{A \%*\% t(X) == t(Y)} and
+#' \code{A.full \%*\% t(X) == t(Y.full)}
+#'
+#' @section Variables:
+#' The list cmu contains the following:
+#' \itemize{
+#'  \item The routing matrix \code{A}. The columns of this matrix correspond to
+#'  individual OD flows (the columns of X), and its rows correspond to individual
+#'  link loads (the columns of Y).
+#'  \item The OD matrix X. Columns correspond to individual OD flows, and the rows
+#'  correspond to observations.
+#'  \item The link load matrix Y. Columns of the Y matrix correspond to individual
+#'  link loads, and the rows correspond to observations.
+#'  \item The routing matrix \code{A.full}. This is the complete routing matrix
+#'  before reduction for full row-rank.
+#'  \item The link load matrix Y.full, corresponding to A.full.
+#' }
+#'
+#' @docType data
+#' @name cmu
+#' @usage cmu
+#' @references A.W. Blocker and E.M. Airoldi. Deconvolution of mixing
+#' time series on a graph. Proceedings of the Twenty-Seventh Conference Annual
+#' Conference on Uncertainty in Artificial Intelligence (UAI-11) 51-60, 2011.
+#' @keywords datasets
+#' @family cmu
+NULL
+
+#' Abilene data from Fang et al. (2007)
+#'
+#' Data from the 12 node Abliene network from Fang et al. (2007). Both the OD
+#' flows and the topology correspond to the actual network. This is the X1
+#' dataset from the given paper.
+#'
+#' @section Objects:
+#' The list abilene, which contains several objects:
+#' \itemize{
+#'  \item \code{A}, the routing matrix for this network (truncated for full row
+#'      rank)
+#'  \item \code{X}, a matrix of origin-destination flows formatted for analysis
+#'  \item \code{Y}, a matrix of link loads formatted for analysis
+#'  \item \code{A.full}, the routing matrix for this network without
+#'  truncatation for full row rank)
+#'  \item \code{Y.full}, a matrix of link loads corresponding to code{A.full}
+#' }
+#' In this data, we have \code{A \%*\% t(X) == t(Y)} and
+#' \code{A.full \%*\% t(X) == t(Y.full)}
+#'
+#' @section Variables:
+#' The list abilene contains the following:
+#' \itemize{
+#'  \item The routing matrix \code{A}. The columns of this matrix correspond to
+#'  individual OD flows (the columns of X), and its rows correspond to individual
+#'  link loads (the columns of Y).
+#'  \item The OD matrix X. Columns correspond to individual OD flows, and the rows
+#'  correspond to observations.
+#'  \item The link load matrix Y. Columns of the Y matrix correspond to individual
+#'  link loads, and the rows correspond to observations.
+#'  \item The routing matrix \code{A.full}. This is the complete routing matrix
+#'  before reduction for full row-rank.
+#'  \item The link load matrix Y.full, corresponding to A.full.
+#' }
+#'
+#' @docType data
+#' @name abilene
+#' @usage abilene
+#' @references J. Fang, Y. Vardi, and C.-H. Zhang. An iterative tomogravity
+#' algorithm for the estimation of network traffic. In R. Liu, W. Strawderman,
+#' and C.-H. Zhang, editors, Complex Datasets and Inverse Problems: Tomography,
+#' Networks and Beyond, volume 54 of Lecture Notes-Monograph Series. IMS, 2007.
+#' @keywords datasets
+#' @family abilene
+NULL
